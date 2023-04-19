7523a4eb9fc16b52a1f411fd8530ec797a0d9db2
tiago-sb <belatiago72@gmail.com>
2023-04-19 20:01:45 -0300

    Create sec1questao03.cpp

---
+++ src/sec1questao03.cpp
@@ -0,0 +1,18 @@
+/*
+ Faca um programa que peca dois numeros e imprima a soma deles.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B;
+
+    cout << "Digite o primeiro valor: ";
+    cin >> A;
+    cout << "Digite o segundo valor: ";
+    cin >> B;
+    cout << "Resultado: " << A  + B << endl;
+
+    return 0;
+}

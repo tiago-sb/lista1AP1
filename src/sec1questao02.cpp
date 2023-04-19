c0891e3dde4e54bbc72746b86b6d07cbedaccf8d
tiago-sb <belatiago72@gmail.com>
2023-04-19 20:01:38 -0300

    Create sec1questao02.cpp

---
+++ src/sec1questao02.cpp
@@ -0,0 +1,15 @@
+/*
+Faca um programa que peca um numero e entao mostre a mensagem:
+“O numero informado foi” [numero].
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double n;
+    cout << "Digite um numero: ";
+    cin >> n;
+    cout << "O numero informado foi [" << n << "]" << endl;
+    return 0;
+}

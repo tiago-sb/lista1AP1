995d990f19e2429a6eec621b856380cee33ee49a
tiago-sb <belatiago72@gmail.com>
2023-04-19 20:01:32 -0300

    Create sec1questao01.cpp

---
+++ src/sec1questao01.cpp
@@ -0,0 +1,12 @@
+/*
+Faca um programa que mostre a mensagem "Alo mundo" na tela.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Alo mundo" << endl;
+    return 0;
+}

8b5673c1b3b3de45fe51a3a222e759f1abe2d993
tiago-sb <belatiago72@gmail.com>
2023-04-25 12:53:57 -0300

    Create sec3questao34.cpp

---
+++ src/sec3questao34.cpp
@@ -0,0 +1,26 @@
+/*
+Faca um programa que leia um nome de usuario e a sua senha e nao aceite a senha igual ao nome
+do usuario, mostrando uma mensagem de erro e voltando a pedir as informacoes.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"portuguese");
+    string nome = "s", senha = "s";
+    cout << "tela de cadastro: " << endl;
+    while(nome == senha){
+        cout << "Insira seu nome: ";
+        cin >> nome;
+        cout << "Insira uma sua senha: ";
+        cin >> senha;
+        if(nome == senha){
+            cout << endl << "ERRO! nome e senhas iguais!\n" << endl;
+        }
+    }
+    system("cls");
+    cout << "Bem vindo(a) ao nosso sistema amigo(a) " << nome << endl;
+
+    return 0;
+}

7b6463167276143ec19289c666ca0608a9f281cb
tiago-sb <belatiago72@gmail.com>
2023-05-01 00:02:25 -0300

    Create sec3questao39.cpp

---
+++ src/sec3questao39.cpp
@@ -0,0 +1,12 @@
+/*
+Faça um programa que leia 5 números e informe o maior número.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    return 0;
+}
